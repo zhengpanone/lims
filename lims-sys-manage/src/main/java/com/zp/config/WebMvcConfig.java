02b94b14c33fdfd11ac5be895806f4e25ce279f5
zhengpanone <zhengpanone@hotmail.com>
2023-03-22 21:05:24 +0800

    uncommit

---
+++ lims-sys-manage/src/main/java/com/zp/config/WebMvcConfig.java
@@ -0,0 +1,31 @@
+package com.zp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * WebMVCConfig
+ * WebMvcConfigurerAdapter配置加入Cors的跨域
+ * @author zhengpanone
+ * @since 2022-11-11
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 跨域配置所有的域名ip都可以
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "DELETE", "PUT") ;
+        //registry.addMapping("/**").allowedOrigins("http://localhost");
+        //开发8080
+        //registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+    }
+}
