44d8643cbef33aa74822108b30b30c5f1552d172
zhengpanqq <129134374+zhengpanqq@users.noreply.github.com>
2023-03-28 16:01:19 +0800

    添加util骨架

---
+++ lims-utils/src/main/java/com/zp/ListUtil.java
@@ -0,0 +1,20 @@
+package com.zp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+* 列表数组工具
+ */
+
+public class ListUtil {
+    
+    
+
+
+
+   
+}
---
+++ lims-utils/src/main/java/com/zp/YmlUtil.java
@@ -0,0 +1,20 @@
+package com.zp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+* application配置获取工具
+ */
+
+public class YmlUtil {
+    
+    
+
+
+
+   
+}
